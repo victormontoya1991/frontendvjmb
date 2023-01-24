1e1d1d1bf0ac308536482d737cc8b3a1001e9c6a
victormontoya1991 <disvictormontoya@gmail.com>
2023-01-23 21:38:15 -0500

    upade font & sass

--- react-frontendvjmb/src/App.js
+++ react-frontendvjmb/src/App.js
@@ -1,10 +1,10 @@
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+          <h1 className='title'> Titulo </h1>
       </header>
     </div>
   );
